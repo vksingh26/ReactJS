9e5ce3bdf33dc7716bb534e28b0ead40bc709ce4
Vikash Kumar Singh <VikashS1@verifone.com>
2020-01-22 12:55:48 +0530

    Added stateful and stateless definition

--- reactapp/src/Learn_React/Learn-React.js
+++ reactapp/src/Learn_React/Learn-React.js
@@ -5,6 +5,9 @@ const learnReact = (props) => {
     // here props is nothing but the property what we are passing from app js LearnReact element.
     // {props.name} this is the way to bind the data dynamically in react
     // {props.children} is the way to bind children dynamically, check app js for children element which is 'my hobby is to ...'
+    //stateful and stateless/dump components
+    //below (return method) is the stateless component as its not maintaining any state
+    //in the next page i.e learn-react.js has the stateful component as it maitains the state of the component.
     return (
         <div>
             <h2>{props.name} made a revenue of {props.revenue}</h2>
