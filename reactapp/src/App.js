aba0ae83773f42cf6d656d83cefa7028aacf0795
Vikash Kumar Singh <VikashS1@verifone.com>
2020-01-23 18:55:35 +0530

    Rendering content conditionaly

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
             {name: "Alipay", revenue: "$35734"},
             {name: "Wechat", revenue: "$44565"},
             {name: "Klarna", revenue: "$46733"}
-        ]
+        ],
+        showContent: false
     }
 
     stateHandler = (newName) =>{
@@ -25,7 +26,7 @@ class App extends Component {
                     name: "Alipay",
                     revenue: "$34467.33"
                 }
-            ]
+            ],
         })
     }
 
@@ -47,6 +48,11 @@ class App extends Component {
         })
     }
 
+    toggleContent = () =>{
+        const doesShow = !this.state.showContent;
+        this.setState({showContent: doesShow});
+    }
+
     render(){
         const buttonStyle = {
             backgroundColor: 'orange',
@@ -58,14 +64,33 @@ class App extends Component {
             color: 'white',
             boxShadow: '2px 5px 5px #ccc'
         }
+
+        let content = null;
+        if(this.state.showContent){
+            content = (
+                <div>
+                    <LearnReact name={this.state.apm[0].name} revenue={this.state.apm[0].revenue}></LearnReact>
+                    <LearnReact name={this.state.apm[1].name} revenue={this.state.apm[1].revenue} 
+                    click={this.stateHandler.bind(this, 'APM: Alipay!!')} change={this.inputChangeHandler}></LearnReact>
+                    <LearnReact name={this.state.apm[2].name} revenue={this.state.apm[2].revenue}></LearnReact>
+                </div>
+            );
+        }
         return (
         <div className="App" >
             <h1> First React App </h1>
             <button style={buttonStyle} onClick={this.stateHandler.bind(this, 'Alipay!!')} > Click To Change State </button>
-            <LearnReact name={this.state.apm[0].name} revenue={this.state.apm[0].revenue}></LearnReact>
-            <LearnReact name={this.state.apm[1].name} revenue={this.state.apm[1].revenue} 
-            click={this.stateHandler.bind(this, 'APM: Alipay!!')} change={this.inputChangeHandler}></LearnReact>
-            <LearnReact name={this.state.apm[2].name} revenue={this.state.apm[2].revenue}></LearnReact>
+            <button style={buttonStyle} onClick={this.toggleContent}>Toggle Type 1</button>
+            { 
+                this.state.showContent ?
+                <div>
+                    <LearnReact name={this.state.apm[0].name} revenue={this.state.apm[0].revenue}></LearnReact>
+                    <LearnReact name={this.state.apm[1].name} revenue={this.state.apm[1].revenue} 
+                    click={this.stateHandler.bind(this, 'APM: Alipay!!')} change={this.inputChangeHandler}></LearnReact>
+                    <LearnReact name={this.state.apm[2].name} revenue={this.state.apm[2].revenue}></LearnReact>
+                </div> : null
+            }
+            {content}
         </div>
         );
         //below line is used to create element using react createelement method, but its kind of cumbersome process if we want to create multiple elements
